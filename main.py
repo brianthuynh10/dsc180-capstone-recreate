7ae76e8458ff08f50068393a6972f9f8eb7e8c74
brianthuynh10 <brianthuynh10@gmail.com>
2025-11-15 12:00:59 -0800

    graphs for sweeps complete - 5 different seeds on 5 different batch sizes

--- main.py
+++ main.py
@@ -1,40 +1,44 @@
 from src import Trainer, clean
 import wandb
-import torch
 
 def main(): 
     # Initialize W&B (sweep agent injects config here)
-    wandb.init(project="vgg16-xray-regression")
+    wandb.init(project="dsc180-capstone-recreate")
 
     # ---- Data Loading ----
     train, val, test, y_mean, y_std = clean()
     print("Data loaded successfully.")
 
     # Read hyperparameters from sweep config
-    bs    = wandb.config.batch_size
-    lr    = wandb.config.lr
-    epochs = wandb.config.epochs
-    seed   = wandb.config.seed
+    batch_size = [8, 16, 32, 64, 128]
+    seeds = [0, 1, 2, 3, 4]
+    lr    =  1e-5
+    epochs = 10
+    # ---- Hyperparameter Sweep Loop ----
+    for bs in batch_size:
+        for seed in seeds:
+            if (bs == 8 and seed !=4) or (bs == 16 and seed == 0): continue
+            else: 
+                # ---- Trainer ----
+                print(f"Starting training with batch size {bs} and seed {seed}")
+                trainer = Trainer(
+                    epochs=epochs,
+                    lr=lr,
+                    batch_size=bs,
+                    seed=seed,
+                    train_dataset=train,
+                    val_dataset=val,
+                    test_dataset=test,
+                    train_mean=y_mean,
+                    train_std=y_std,
+                    run_name=f"bs{bs}-seed{seed}"
+                )
 
-    # ---- Trainer ----
-    trainer = Trainer(
-        epochs=epochs,
-        lr=lr,
-        batch_size=bs,
-        seed=seed,
-        train_dataset=train,
-        val_dataset=val,
-        test_dataset=test,
-        train_mean=y_mean,
-        train_std=y_std,
-        run_name=f"bs{bs}-seed{seed}"
-    )
-
-    wandb.watch(trainer.model, log="all", log_freq=100)
-    
-    trainer.train()
-    trainer.evaluate()
-    wandb.finish()
+                wandb.watch(trainer.model, log="all", log_freq=100)
+                
+                trainer.train()
+                trainer.evaluate()
+                wandb.finish()
 
 if __name__ == "__main__":
     main()
